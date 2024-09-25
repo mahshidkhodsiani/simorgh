7a4e0d1e75007a867b8b912cc48c2e68a984e868
unknown <mahshidkhodsiani@yahoo.com>
2024-09-25 17:30:00 +0330

    054-sim

--- test.php
+++ test.php
@@ -1,4 +1,39 @@
 <?php
 
-echo "<pre>";
-var_dump($_SERVER);
+class mahshid {
+    const FAMILY = "my family is khodsiani  ";
+}
+
+$girl = new Mahshid ();
+
+echo $girl::FAMILY;
+
+
+echo "<br>";
+echo "<br>";
+
+
+
+abstract class ParentClass {
+    // Abstract method with an argument
+    abstract protected function prefixName($name);
+  }
+  
+  class ChildClass extends ParentClass {
+    // The child class may define optional arguments that is not in the parent's abstract method
+    public function prefixName($name, $separator = ".", $greet = "Dear") {
+      if ($name == "John Doe") {
+        $prefix = "Mr";
+      } elseif ($name == "Jane Doe") {
+        $prefix = "Mrs";
+      } else {
+        $prefix = "";
+      }
+      return "{$greet} {$prefix}{$separator} {$name}";
+    }
+  }
+  
+  $class = new ChildClass;
+  echo $class->prefixName("John Doe");
+  echo "<br>";  
+  echo $class->prefixName("Jane Doe");
