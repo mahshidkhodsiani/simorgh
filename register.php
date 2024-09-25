3e38d2ba04525d7dab8a58500336305fd3bc4bbc
unknown <mahshidkhodsiani@yahoo.com>
2024-09-25 16:55:50 +0330

    053-sim

--- register.php
+++ register.php
@@ -134,8 +134,8 @@ session_start();
 <?php
 
 
-
-if (!empty($_POST)) {
+// var_dump(empty($_POST));
+if (isset($_POST)) {
     $CurUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $CurUrl = substr($CurUrl, 0, strrpos($CurUrl, '/') + 1);
 
